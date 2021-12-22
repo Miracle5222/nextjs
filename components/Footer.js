70557477f2b4cab2edeaebf381b088411c6a2bcc
Roneil Bansas <Roneilbansas5222@gmail.com>
2021-12-22 06:33:41 -0800

    adding custom 404 Page

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <footer>
+            Copyright 2021 Name List
+        </footer>
+    )
+}
+
+export default Footer
---
+++ pages/names/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+
+const Names = () => {
+  return (
+    <div>
+    
+      <h1>Name List</h1>
+      <ul>
+        <li>Roneil</li>
+        <li>Bansas</li>
+        <li>Gungob</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Names;
