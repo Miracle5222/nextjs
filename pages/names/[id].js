112d7e435bba0221ff0cf035591d0d2924551d1e
Roneil Bansas <Roneilbansas5222@gmail.com>
2021-12-22 23:17:09 -0800

    deploy

--- pages/names/[id].js
+++ pages/names/[id].js
@@ -1,7 +1,35 @@
-const Details = () => {
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  const paths = data.map((named) => {
+    return {
+      params: { id: named.id.toString() },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+  const data = await res.json();
+
+  return {
+    props: { named: data },
+  };
+};
+
+const Details = ({named}) => {
   return (
     <div>
-      <h1>Details page</h1>
+          <h1>{named.name}</h1>
+          <p>{named.email}</p>
+          <p>{named.website}</p>
+          <p>{named.address.city}</p>
     </div>
   );
 };
